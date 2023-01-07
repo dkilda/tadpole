5b8844f1fc52265d50ff65fbf2714624ed5fab56
kilda <dainius.kilda@gmail.com>
2023-01-07 11:04:08 +0100

    adding code for arg glue and arg pack

--- tadpole/autodiff/graph.py
+++ tadpole/autodiff/graph.py
@@ -4,9 +4,118 @@
 import abc
 
 from tadpole.autodiff.node import make_node
+from tadpole.autodiff.node import Node, Point
 
 from tadpole.autodiff.util import cacheable
 from tadpole.autodiff.util import Stack
+
+
+
+
+###############################################################################
+###                                                                         ###
+###  Argument glue and packs: glueing arguments into a single pack          ###
+###                           for function calls.                           ###
+###                                                                         ###
+###############################################################################
+
+
+# --- Argument glue --------------------------------------------------------- #
+
+class ArgGlue:
+
+   def __init__(self, args):
+
+       self._args = args
+
+
+   def _filter(self, mask):
+
+       return tuple(arg for arg in self._args if mask(arg))
+
+
+   def _vals(self):
+
+       return self._filter(lambda x: not isinstance(x, Node)) 
+
+
+   def _nodes(self):
+
+       return self._filter(lambda x: isinstance(x, Node)) 
+
+
+   def _packof(self, nodes, funcall):
+
+       funcall = funcall.add(*self._vals())
+
+       if len(nodes) > 0: 
+          return nodes[0].glue(nodes[1:])
+                         .pack(funcall)
+
+       return EmptyPack(funcall)
+
+
+   def nodepack(self, funcall):
+
+       return self._packof(self._nodes(), funcall)
+
+
+   def pointpack(self, funcall):
+
+       points = [node.topoint() for node in self._nodes()] 
+       return self._packof(points, funcall)
+
+
+
+
+# --- Argument pack --------------------------------------------------------- #
+
+class ArgPack:
+
+   def __init__(self, args):
+
+       self._args = args
+
+
+   def nodepack(self): 
+
+       return ArgGlue(self._args).nodepack(FunCall())
+
+
+   def pointpack(self):
+
+       return ArgGlue(self._args).pointpack(FunCall())
+
+
+
+
+# --- Function call --------------------------------------------------------- #
+
+class FunCall:
+
+   def __init__(self, args=None):
+
+       if args is None:
+          args = Stack()
+
+       self._args = args
+
+
+   def add(self, *args):
+
+       xs = self._args
+
+       for arg in args:
+           xs = xs.push(arg)
+
+       return self.__class__(xs)
+
+
+   def execute(self, fun):
+
+       return fun(*self._args.riter())
+
+
 
 
 ###############################################################################
@@ -55,7 +164,7 @@ class Sources:
 class Glue(abc.ABC):
 
    @abc.abstractmethod
-   def pack(self, fun):
+   def pack(self, funcall):
        pass
 
 
@@ -148,9 +257,12 @@ class NodePack(Pack):
 class PointPack(Pack):
 
    def __init__(self, source):
+
        self._source = source
 
+
    def pluginto(self, fun):
+
        return self._source.pluginto(fun)
 
       
@@ -161,39 +273,18 @@ class PointPack(Pack):
 class EmptyPack(Pack):
 
    def __init__(self, funcall):
+
        self._funcall = funcall
 
+
    def pluginto(self, fun):
+
        return self._funcall.execute(fun)
 
 
 
 
-# --- Function call --------------------------------------------------------- #
 
-class FunCall:
-
-   def __init__(self, args=None):
-
-       if args is None:
-          args = Stack()
-
-       self._args = args
-
-
-   def add(self, *args):
-
-       xs = self._args
-
-       for arg in args:
-           xs = xs.push(arg)
-
-       return self.__class__(xs)
-
-
-   def execute(self, fun):
-
-       return fun(*self._args.riter())
 
 
 
