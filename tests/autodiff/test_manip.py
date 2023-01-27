fdbff9c617885932eb21bbaa5f00ee21c51220c0
kilda <dainius.kilda@gmail.com>
2023-01-27 10:40:11 +0100

    adding tests for manip module

--- tests/autodiff/test_manip.py
+++ tests/autodiff/test_manip.py
@@ -8,8 +8,22 @@ import tests.autodiff.fakes   as fake
 import tests.common.ntuple as tpl
 
 
+# --- Wrapper for gradient addition ----------------------------------------- #
+
+class TestAddGrads:
+
+   @pytest.mark.parametrize("net_g, g", [
+      [fake.CumFunReturn(), fake.CumFunReturn()],
+   ])
+   def test_add_grads(self, net_g, g):
+
+       assert tdmanip.add_grads(net_g, g) == net_g + g
 
 
+   @pytest.mark.parametrize("g", [fake.CumFunReturn()])
+   def test_add_grads_simple(self, g):
+
+       assert tdmanip.add_grads(None, g) == g
 
 
 
