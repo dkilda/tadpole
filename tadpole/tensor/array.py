f1681b530f80f17c1bf4bd988be4a94da935916c
kilda <dainius.kilda@gmail.com>
2023-03-18 13:26:14 +0100

    implementing VoidArray

--- tadpole/tensor/array.py
+++ tadpole/tensor/array.py
@@ -31,7 +31,150 @@ class ArrayLike(abc.ABC):
    @abc.abstractmethod
    def __or__(self, other):
        pass
+
+
+
+
+###############################################################################
+###                                                                         ###
+###  Definition of Void Array (supports array creation)                     ###
+###                                                                         ###
+###############################################################################
+
+
+# --- Void-Array ------------------------------------------------------------ #
+
+class VoidArray(ArrayLike):
+
+   # --- Construction --- #
+
+   def __init__(self, backend):
+
+       self._backend = backend
+
+
+   # --- Arraylike methods --- #
+
+   def new(self, data):
+
+       return OneArray(self._backend, data)
+
+
+   def __or__(self, other):
+
+       return other 
+
+
+   # --- Array creation methods --- #
+
+   def asarray(self, array, **opts):
+
+       if isinstance(array, ArrayLike):
+          return array
+
+       data = self._backend.asarray(array, **opts)
+
+       return self.new(data)
+
+
+   def zeros(self, shape, **opts):
+
+       data = self._backend.zeros(shape, **opts)
+
+       return self.new(data)
+
+
+   def ones(self, shape, **opts):
+
+       data = self._backend.ones(shape, **opts)     
+
+       return self.new(data)     
+
+
+   def unit(self, shape, idx, **opts):
+
+       data = self._backend.unit(shape, idx, **opts)
+
+       return self.new(data)
+       
+
+   def eye(self, N, M=None, **opts):
+
+       data = self._backend.eye(N, M, **opts)
+
+       return self.new(data)
+       
+
+   def rand(self, shape, **opts):
+
+       data = self._backend.rand(shape, **opts)
+
+       return self.new(data)
+       
+
+   def randn(self, shape, **opts):
+
+       data = self._backend.randn(shape, **opts)
+
+       return self.new(data)
+       
+
+   def randuniform(self, shape, boundaries, **opts):
+
+       data = self._backend.randuniform(shape, boundaries, **opts)
+
+       return self.new(data)
+
    
+
+
+###############################################################################
+###                                                                         ###
+###  Standalone functions corresponding to VoidArray methods                ###
+###                                                                         ###
+###############################################################################
+
+
+# --- Array creation methods ------------------------------------------------ #
+
+def asarray(x, array, **opts):
+
+    return x.asarray(array, **opts)
+
+
+def zeros(x, shape, **opts):
+
+    return x.zeros(shape, **opts)
+
+
+def ones(x, shape, **opts):
+
+    return x.ones(shape, **opts)
+
+
+def unit(x, shape, idx, **opts):
+
+    return x.unit(shape, idx, **opts)
+
+
+def eye(x, N, M=None, **opts):
+
+    return x.eye(N, M=None, **opts)
+
+
+def rand(x, shape, **opts):
+
+    return x.rand(shape, **opts)
+
+
+def randn(x, shape, **opts):
+
+    return x.randn(shape, **opts)
+       
+
+def randuniform(x, shape, boundaries, **opts):
+
+    return x.randn(shape, boundaries, **opts)
 
 
 
@@ -257,7 +400,14 @@ class OneArray(ArrayLike):
 
        data = self._backend.argsort(self._data, axis, **opts)
 
-       return self.new(data)         
+       return self.new(data)   
+
+
+   def diag(self, **opts):
+
+       data = self._backend.diag(self._data, **opts)
+
+       return self.new(data)      
 
 
    # --- Standard math --- #
@@ -594,6 +744,11 @@ def put(x, idxs, vals, accumulate=False):
 def argsort(x, axis=-1, **opts):
 
     return x.argsort(axis, **opts)
+
+
+def diag(x, **opts):
+
+    return x.diag(**opts)
 
     
 
