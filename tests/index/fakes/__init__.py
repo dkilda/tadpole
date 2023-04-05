24badb51def8308694e56ff98cc64939982886d0
kilda <dainius.kilda@gmail.com>
2023-04-05 13:11:18 +0200

    cleaning up, creating a new test directory for index subpackage

---
+++ tests/index/fakes/__init__.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from tests.util.fakes import (
+   Map,
+   Value,
+   Fun,
+   FunMap,
+   Op,   
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tests/index/test_index.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+import collections
+import itertools
+import numpy as np
+
+import tadpole.util  as util
+import tadpole.index as tid
+
+import tests.index.fakes as fake
+import tests.index.data  as data
+
+
+from tadpole.index import (
+   Index,
+   IndexGen,  
+   Indices,
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
