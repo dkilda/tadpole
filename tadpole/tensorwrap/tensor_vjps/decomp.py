e94833d42e6db21c58209290b10ca16f5cfe42ce
kilda <dainius.kilda@gmail.com>
2023-04-07 11:56:47 +0200

    new module for decomp vjps

---
+++ tadpole/tensorwrap/tensor_vjps/decomp.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import tadpole.util     as util
+import tadpole.autodiff as ad
+import tadpole.tensor   as tn
+
+
+from tadpole.index import (
+   Index,
+   IndexGen, 
+   Indices,
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
