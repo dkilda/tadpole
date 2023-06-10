812fd4e6b9eb7fa27e72b2a97af4b7f7484bae2b
kilda <dainius.kilda@gmail.com>
2023-06-10 20:16:38 +0200

    minor adjustments

--- examples/fluids.py
+++ examples/fluids.py
@@ -14,16 +14,30 @@ import os
 import numpy   as np
 import tadpole as td
 
-
-
-def project():
-
-
-
-
+from tadpole import (
+   IndexGen,
+   IndexLit,
+)
 
 
 
+'''
+# def project(vx, vy):
+# ix, iy = np.meshgrid(np.arange(3), np.arange(4))
+
+    
+x   = td.randn((IndexLit("i",3), IndexLit("i",4)))
+idx = (ix, iy) #(1, slice(2))
+
+
+def fun(x, idx):
+    return x[idx]
+
+
+print("Value: ", td.asdata(fun(x, idx)))
+print("Grad:  ", td.asdata(td.gradient(fun)(x, idx)))
+
+'''
 
 
 
